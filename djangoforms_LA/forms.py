7a51d16d362b25e183655a4e8ca29cc32c31be30
emobilis <lawrieabuna@gmail.com>
2023-02-23 10:04:09 +0300

    styled the form

--- djangoforms_LA/forms.py
+++ djangoforms_LA/forms.py
@@ -2,10 +2,17 @@ from django import forms
 
 
 class UserReg(forms.Form):
-    name = forms.CharField(max_length=100,min_length=1)
+    name = forms.CharField(max_length=100)
     email = forms.CharField(max_length=100)
     password = forms.CharField()
 
 class UserLogin(forms.Form):
     username = forms.CharField(max_length=100)
     user_password = forms.CharField()
+
+class Membersreg(forms.Form):
+    name = forms.CharField(max_length=100)
+    age = forms.CharField(max_length=100)
+    phone = forms.CharField(max_length=100)
+    city = forms.CharField(max_length=100)
+    country = forms.CharField(max_length=100)
